f6f56d9c1964be5b0113687c0ac401bcd84415e5
Mahsa Khamisi <mahsa.khamisi@me.com>
2020-04-08 18:11:45 +0200

    Added autocomplete for location

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,6 +24,10 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import {initAutocomplete} from '../plugins/init_autocomplete'
+
+
+
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -32,3 +36,5 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
 });
+
+initAutocomplete();
