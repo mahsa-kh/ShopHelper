63f41830bc64125d10f37f6d00466c2d143f144c
Mahsa Khamisi <mahsa.khamisi@me.com>
2020-04-09 16:31:34 +0200

    Create shopping list with items-and view the list

--- app/models/shopping_list.rb
+++ app/models/shopping_list.rb
@@ -1,6 +1,5 @@
 class ShoppingList < ApplicationRecord
   belongs_to :user
-  belongs_to :driver, through: :order
   has_many :orders
   has_many :items, dependent: :destroy
   validates :status, presence: true
