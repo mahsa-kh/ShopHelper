0314c5f661f1c9f6dfbe083bdf7518b8a22846dc
Massimo Galeotti <massi.galex@gmail.com>
2020-04-09 18:43:02 +0200

    created controllers with empty action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,22 +2,12 @@ class OrdersController < ApplicationController
   before_action :set_order, only: [:show, :edit, :update, :destroy]
 
   def index
-    @orders = Order.all?
   end
 
   def new
-    @order = Order.new
   end
 
   def create
-    @order = Order.new(order_params)
-    @order.user = current_user
-
-    if order.save
-      redirect_to @order, notice: "the order was successfully created"
-    else
-      render :new
-    end
   end
 
   def show
@@ -27,26 +17,17 @@ class OrdersController < ApplicationController
   end
 
   def update
-    if @order.update(order_params)
-      redirect_to @order, notice: "Order was successfully update"
-    else
-      render :new
-    end
   end
 
   def destroy
-    @order.destroy
-    redirect_to order_url, notice: "the order was successfully destroyed"
   end
 
 
   private
 
   def set_order
-    @order = Order.find(params[:id])
   end
 
   def order_params
-    params.require(:order).permit(:name)
   end
 end
--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,23 +1,13 @@
 class ShoppingListsController < ApplicationController
-  before_action :set_shopping_list, only: [:show, :edit, :update, :destroy]
+  before_action :set_order, only: [:show, :edit, :update, :destroy]
 
   def index
-    @shopping_lists = Shopping_list.all?
   end
 
   def new
-    @shopping_list = Shopping_list.new
   end
 
   def create
-    @shopping_list = Shopping_list.new(shopping_list_params)
-    @shopping_list.user = current_user
-
-    if shopping_list.save
-      redirect_to @shopping_list, notice: "the shopping list was successfully created"
-    else
-      render :new
-    end
   end
 
   def show
@@ -27,26 +17,17 @@ class ShoppingListsController < ApplicationController
   end
 
   def update
-    if @shopping_list.update(shopping_list_params)
-      redirect_to @shopping_list, notice: "Shopping list was successfully update"
-    else
-      render :new
-    end
   end
 
   def destroy
-    @shopping_list.destroy
-    redirect_to shopping_list_url, notice: "the shopping list was successfully destroyed"
   end
 
 
   private
 
-  def set_shopping_list
-    @shopping_list = Shopping_list.find(params[:id])
+  def set_order
   end
 
-  def shoppig_list_params
-    params.require(:shopping_list).permit(:name)
+  def order_params
   end
 end
