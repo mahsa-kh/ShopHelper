b025736383d25f01634e6fe372aca8131607fc6f
Mahsa Khamisi <mahsa.khamisi@me.com>
2020-04-10 12:11:34 +0200

    merge changes

--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,48 @@
 
 ActiveRecord::Schema.define(version: 2020_04_09_061146) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+
+
+ # These are extensions that must be enabled in order to support this database  enable_extension "plpgsql"
+
+
+  create_table "drivers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "location"
+    t.boolean "available", default: false
+    t.integer "rating"
+    t.index ["email"], name: "index_drivers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_drivers_on_reset_password_token", unique: true
+  end
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+
 
   create_table "drivers", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -59,7 +99,10 @@ ActiveRecord::Schema.define(version: 2020_04_09_061146) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_shopping_lists_on_user_id"
+
+
   end
+
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -78,6 +121,7 @@ ActiveRecord::Schema.define(version: 2020_04_09_061146) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "shopping_lists"
   add_foreign_key "orders", "drivers"
   add_foreign_key "orders", "shopping_lists"
