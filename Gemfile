a6dd5f116de8e14b48b53e8c10d63d9a1719c59d
Massimo Galeotti <massi.galex@gmail.com>
2020-04-10 14:58:40 +0200

    did smt

--- Gemfile
+++ Gemfile
@@ -40,7 +40,7 @@ group :development, :test do
   gem 'pry-byebug'
   gem 'pry-rails'
   gem 'dotenv-rails'
-  
+
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
@@ -52,6 +52,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'pundit'
 end
 
 group :test do
--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,10 +1,19 @@
 class ShoppingListsController < ApplicationController
   before_action :set_order, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user! except: :index
+
+  after_action :verify_authorized, except: :index
+  after_action :verify_policy_scoped, only: :index
+
+
 
   def index
+    @shopping_list = policy_scope(Shopping_list).reverse
   end
 
   def new
+    @shopping_list = current_user.shopping_list.new
+    authorize @shopping_list
   end
 
   def create
@@ -25,9 +34,10 @@ class ShoppingListsController < ApplicationController
 
   private
 
-  def set_order
+  def set_shopping_list
+    authorize @shopping_list
   end
 
-  def order_params
+  def shopping_list_params
   end
 end
