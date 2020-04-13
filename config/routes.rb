e97eff3a4530e2a1630f9e79461acc4e0b3e2408
Mahsa Khamisi <mahsa.khamisi@me.com>
2020-04-13 19:51:30 +0200

    started view_all shopping list

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,11 @@ Rails.application.routes.draw do
   devise_for :drivers, path: 'drivers'
   # eg. http://localhost:3000/admins/sign_in
 
-  resources :shopping_lists, only: [:index, :new, :create, :show, :edit, :update, :picks, :view_all] do
+  resources :shopping_lists, only: [:index, :new, :create, :show, :edit, :update, :picks] do
     resources :items, only: [:new, :create, :show]
   end
 
+  get "/shopping_lists/view_all", to: "shopping_lists#view_all", as: "view_all"
   get "/orders/:id/create_order", to: "orders#create_order", as: "create_order"
   get "/orders/:driver_id/picks", to: "orders#picks", as: "picks"
   post "/orders/:id/deliver", to: "orders#mark_as_delivered", as: "order_delivered"
