85984694bbfa047165dde4221b1be4e49f6517d4
Daniel Polo <od.polo@gmail.com>
2020-04-15 04:39:30 -0500

    Auth fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
-
-  before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_driver!
-
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
