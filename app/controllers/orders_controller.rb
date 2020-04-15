7d5e57f74235d868d3c394cb1d4bf99c7fc617b2
Andrea Tota <andrea.tota6@gmail.com>
2020-04-15 17:25:04 +0200

    Fix style of orders page. Display cards similar to shopping_lists and added map to visualize the location of the pick

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,6 +13,14 @@ class OrdersController < ApplicationController
 
   def picks
     @orders = Order.where("driver_id = ?", params[:driver_id])
+
+    @markers = @orders.map do |order|
+      {
+        lat: order.user.latitude,
+        lng: order.user.longitude,
+        infoWindow: render_to_string(partial: "shopping_lists/info_window", locals: { user: order.user })
+      }
+    end
   end
 
   def create_order
