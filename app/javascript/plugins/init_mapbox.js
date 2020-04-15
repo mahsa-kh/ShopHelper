9ab7fd746fe31c0f874c4ab4556958fa08f56e87
Rodolfo Pinto <rodolfopintomail@gmail.com>
2020-04-15 15:46:35 +0200

    fitMapToMarkers done

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -9,61 +9,71 @@ const initMapbox = () => {
     const bounds = new mapboxgl.LngLatBounds();
     markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
     map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
-  };
+    };
 
-  const mapElement = document.getElementById("map");
-  console.log(mapElement);
-  if (mapElement) {
-  	mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-    const map = new mapboxgl.Map({
+    const mapElement = document.getElementById("map");
+    console.log(mapElement);
+  
+    if (mapElement) {
+  	  mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+      const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
-    });
-    const markers = JSON.parse(mapElement.dataset.markers)
-    markers.forEach(marker => {
-    	const popup = new mapboxgl.Popup().setHTML(marker.infoWindow)
-    	const customizedMarker = document.createElement('div');
-		  customizedMarker.className = 'marker';
-		  customizedMarker.style.backgroundImage = `url('${marker.picture}')`;
-		  customizedMarker.style.backgroundSize = 'contain';
-		  customizedMarker.style.width = '25px';
-		  customizedMarker.style.height = '25px';
-    	new mapboxgl.Marker()
-    	.setLngLat([ marker.lng, marker.lat ])
-    	.setPopup(popup)
-    	.addTo(map)
-    })
+      });
+    
+      const markers = JSON.parse(mapElement.dataset.markers)
+    
+      markers.forEach(marker => {
+    	  const popup = new mapboxgl.Popup().setHTML(marker.infoWindow)
+    	  const customizedMarker = document.createElement('div');
+		    customizedMarker.className = 'marker';
+		    customizedMarker.style.backgroundImage = `url('${marker.picture}')`;
+		    customizedMarker.style.backgroundSize = 'contain';
+		    customizedMarker.style.width = '25px';
+		    customizedMarker.style.height = '25px';
+    	  new mapboxgl.Marker()
+    	  .setLngLat([ marker.lng, marker.lat ])
+    	  .setPopup(popup)
+    	  .addTo(map)
+      })
 
       const main = async () => {
-  const position1 = await getCurrentPosition({
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0,
-  });
+      const position1 = await getCurrentPosition({
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+      });
 
+       if (position1) { 
+         const locationPin = document.createElement('div');
+         locationPin.className = 'marker';
+         locationPin.style.backgroundColor = 'red';
+         locationPin.style.width = '12px';
+         locationPin.style.height = '12px';
+         locationPin.style.borderRadius = '6px'; 
+         new mapboxgl.Marker(locationPin)
+         .setLngLat([ position1.coords.longitude, position1.coords.latitude ])
+         .addTo(map)
+         const url = `${location.href}?lat=${position1.coords.latitude}&lng=${position1.coords.longitude}`;
+         const currentUrl = new URL(window.location.href);
+           if (!(currentUrl.searchParams.get('lat'))) {
+             window.location = url;
+           }  
+        
+          markers.push( { 
+            lat: position1.coords.latitude,
+            lng: position1.coords.longitude
+          })
 
+          fitMapToMarkers(map, markers)
 
-  new mapboxgl.Marker()
-   
-      .setLngLat([ position1.coords.longitude, position1.coords.latitude ])
-      .addTo(map)
-      
-      const url = `${location.href}?lat=${position1.coords.latitude}&lng=${position1.coords.longitude}`;
-      const currentUrl = new URL(window.location.href);
+        }
+    };
 
-      if (!(currentUrl.searchParams.get('lat'))) {
-        window.location = url;
-      }
-     
-  
-};
-main();
-
-
+    main();
 
     console.log(markers)
-
-       fitMapToMarkers(map, markers)
+    fitMapToMarkers(map, markers)
   //   map.addControl(new MapBoxGeocoder({
   //     accessToken: mapboxgl.accessToken,
   //     mapboxgl: mapboxgl
