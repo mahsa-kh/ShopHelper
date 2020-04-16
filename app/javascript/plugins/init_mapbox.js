1773255d566ba99e9d44a1839e0968d68e475298
Rodolfo Pinto <rodolfopintomail@gmail.com>
2020-04-16 15:49:09 +0200

    Buttons and Map style completed

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -18,7 +18,7 @@ const initMapbox = () => {
   	  mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
       const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/rodolfop/ck92sn75k2gw51imqq8xjgv2s'
       });
     
       const markers = JSON.parse(mapElement.dataset.markers)
